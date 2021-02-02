942f6bebe818862d45fde2eedd89912390d32035
crack <corbin@mathematik.uni-kl.de>
2021-02-02 22:51:21 +0100

    use the with open idiom

--- create_marked_exams.py
+++ create_marked_exams.py
@@ -3,6 +3,7 @@ import copy
 import PyPDF2
 import argparse
 import logging
+import sys
 
 from defaultlogger import set_default_logging_behavior
 logger = logging.getLogger('medocr.create_marked_exams')
@@ -28,34 +29,35 @@ if __name__ == '__main__':
     parser.add_argument('--output', '-o', type=str, default=None, help='The folder for outputs')    
     args = parser.parse_args()
     
-    validate_pdf_file_name(args.exam)
-    validate_pdf_file_name(args.marks)
-    
-    exam_name, ext = os.path.splitext(args.exam)
-    
-    if args.output is None:
-        args.output = exam_name
-    
-    exam_file = open(args.exam, 'rb')
-    exam_pdf_reader = PyPDF2.PdfFileReader(exam_file)
-    
-    marks_file = open(args.marks, 'rb')
-    marks_pdf_reader = PyPDF2.PdfFileReader(marks_file)
-     
-    os_utils.mkdir_if_nonexistent(args.output)   
-    
-    for idx in range(marks_pdf_reader.numPages): 
-        out_writer = PyPDF2.PdfFileWriter()
-        mark_page = marks_pdf_reader.getPage(idx)
-        for exam_page_number in range(exam_pdf_reader.numPages):
-            exam_page_original = exam_pdf_reader.getPage(exam_page_number)
-            exam_page = copy.copy(exam_page_original)
-            exam_page.mergePage(mark_page)
-            out_writer.addPage(exam_page)
-        out_file_name = '{0}_{1:03d}.pdf'.format(exam_name, idx)
-        out_file = open(os.path.join(args.output, out_file_name), 'wb')
-        out_writer.write(out_file)
-        out_file.close()
+    try: 
+        validate_pdf_file_name(args.exam)
+        validate_pdf_file_name(args.marks)
         
-    exam_file.close()
-    marks_file.close()
+        exam_name, ext = os.path.splitext(args.exam)
+        
+        if args.output is None:
+            args.output = exam_name
+        os_utils.mkdir_if_nonexistent(args.output)   
+                
+        
+        with open(args.exam, 'rb') as exam_file: 
+            with  open(args.marks, 'rb') as marks_file:
+                exam_pdf_reader = PyPDF2.PdfFileReader(exam_file)
+                marks_pdf_reader = PyPDF2.PdfFileReader(marks_file)
+                
+                for idx in range(marks_pdf_reader.numPages): 
+                    out_writer = PyPDF2.PdfFileWriter()
+                    mark_page = marks_pdf_reader.getPage(idx)
+                    for exam_page_number in range(exam_pdf_reader.numPages):
+                        exam_page_original = exam_pdf_reader.getPage(exam_page_number)
+                        exam_page = copy.copy(exam_page_original)
+                        exam_page.mergePage(mark_page)
+                        out_writer.addPage(exam_page)
+                    out_file_name = '{0}_{1:03d}.pdf'.format(exam_name, idx)
+                    with open(os.path.join(args.output, out_file_name), 'wb') as out_file:
+                        out_writer.write(out_file)
+
+        
+    except Exception as ex:
+        logger.critical('', exc_info=ex)
+        sys.exit(1)
