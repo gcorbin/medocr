4fc44381c16e60c9017cf36f2dd2906f1ebed48c
crack <corbin@mathematik.uni-kl.de>
2021-03-05 10:38:13 +0100

    rotating logfiles, to keep the last five logs

--- medocr.py
+++ medocr.py
@@ -3,6 +3,7 @@ import argparse
 import logging
 import sys
 import os_utils
+import time
 from defaultlogger import set_default_logging_behavior
 
 from collection import Collection
@@ -25,8 +26,31 @@ def find_free_path(raw_path):
     return full_path
 
 
+def find_most_recent_log_number(path):
+    logfiles = [name_ext for name_ext in os.listdir(path) if name_ext.startswith('medocr') and name_ext.endswith('.log')]
+    most_recent_number = 0
+    most_recent_time = 0  # start of the epoch
+    for name_ext in logfiles:
+        name, ext = os.path.splitext(name_ext)
+        tmp = name.split('-')
+        if len(tmp) != 2:
+            continue
+        number_string = tmp[1]
+        try:
+            number = int(number_string)
+        except ValueError:
+            continue
+        modification_time = os.stat(os.path.join(path, name_ext)).st_mtime
+        if modification_time > most_recent_time:
+            most_recent_time = modification_time
+            most_recent_number = number
+    return most_recent_number
+
+
 if __name__ == '__main__':
-    set_default_logging_behavior(logfile='medocr')
+    prev_log_number = find_most_recent_log_number('.')
+    log_number = (prev_log_number % 5) + 1
+    set_default_logging_behavior(logfile='medocr-{:d}'.format(log_number))
 
     logger.debug('Parsing arguments')
     parent_parser = argparse.ArgumentParser()
